7fe5cac8d722e9731f1d53f03d91eaaed8054138
Landon <landon.holmes@gmail.com>
2015-12-10 15:10:34 -0600

    WIP: trying to dynamically include a header .not going well

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+
+var headerData;
+fs.readFile("./views/includes/header.html", function(err, data){
+  if (err) return console.error(err);
+  headerData = data;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', header: headerData });
 });
 
 module.exports = router;
