c27a4f442971d3995e44755cd3eaadb351bc85f4
Landon Holmes <landon.holmes@gmail.com>
2015-12-11 06:02:40 -0600

    I gave up on the included header. this is off to a wonderful start...

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-var headerData;
-fs.readFile("./views/includes/header.html", function(err, data){
-  if (err) return console.error(err);
-  headerData = data;
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', header: headerData });
+    res.render('index', { title: 'Nodetroller'});
 });
 
 module.exports = router;
