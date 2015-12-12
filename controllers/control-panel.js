2a014fa6fed418cb73c8f30ad562c1230809c7a8
Landon Holmes <landon.holmes@gmail.com>
2015-12-11 21:42:02 -0600

    added another thing for restarting the mc server

--- controllers/control-panel.js
+++ controllers/control-panel.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var utils = require('..//helpers/utils');
+var utils = require('..//helpers/pass');
 
 
 router.get('/', function(req, res, next) {
@@ -9,17 +10,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/listDir', function(req, res){
-    listDir(function(r){
+    utils.run_cmd('ls',['-la'],function(r) {
         res.send(r);
     });
 });
 
-
-function listDir(callback) {
-    utils.run_cmd('ls',['-la'],function(r) {
-        callback(r);
+router.get('/restartMinecraftServer', function(req, res){
+    utils.run_cmd('systemctl',['restart','minecraftserver'],function(r) {
+        res.send(r);
     });
-}
+});
+
 
 
 
