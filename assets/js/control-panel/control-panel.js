ea0fcdd16606e2184f5e0aad647315873b0a33d3
Landon Holmes <landon.holmes@gmail.com>
2015-12-12 18:33:49 -0600

    added timestamp displays to control panel for minecraft server operations

--- assets/js/control-panel/control-panel.js
+++ assets/js/control-panel/control-panel.js
@@ -19,7 +19,8 @@ var restartMinecraftServer = function(e){
     $.get( '/restartMinecraftServer',{}, function(data) {
         var outputDiv = buttonClicked.parents(".panel-footer").siblings(".panel-body").children(".output");
         data = data.replace(/\n/g, "<br/>");
-        outputDiv.append(data+'<br />');
+        //outputDiv.append(data+'<br />');
+        outputDiv.append(getTimestamp()+": Restarting server...<br />");
         outputDiv.scrollTop(outputDiv[0].scrollHeight);
     });
 };
@@ -29,7 +30,8 @@ var stopMinecraftServer = function(e){
     $.get( '/stopMinecraftServer',{}, function(data) {
         var outputDiv = buttonClicked.parents(".panel-footer").siblings(".panel-body").children(".output");
         data = data.replace(/\n/g, "<br/>");
-        outputDiv.append(data+'<br />');
+        //outputDiv.append(data+'<br />');
+        outputDiv.append(getTimestamp()+": Stopping server...<br />");
         outputDiv.scrollTop(outputDiv[0].scrollHeight);
     });
 };
@@ -39,7 +41,8 @@ var startMinecraftServer = function(e){
     $.get( '/startMinecraftServer',{}, function(data) {
         var outputDiv = buttonClicked.parents(".panel-footer").siblings(".panel-body").children(".output");
         data = data.replace(/\n/g, "<br/>");
-        outputDiv.append(data+'<br />');
+        //outputDiv.append(data+'<br />');
+        outputDiv.append(getTimestamp()+": Starting server...<br />");
         outputDiv.scrollTop(outputDiv[0].scrollHeight);
     });
 };
@@ -49,7 +52,8 @@ var enableMinecraftServer = function(e){
     $.get( '/enableMinecraftServer',{}, function(data) {
         var outputDiv = buttonClicked.parents(".panel-footer").siblings(".panel-body").children(".output");
         data = data.replace(/\n/g, "<br/>");
-        outputDiv.append(data+'<br />');
+        //outputDiv.append(data+'<br />');
+        outputDiv.append(getTimestamp()+": Enabling server...<br />");
         outputDiv.scrollTop(outputDiv[0].scrollHeight);
     });
 };
@@ -59,13 +63,14 @@ var disableMinecraftServer = function(e){
     $.get( '/disableMinecraftServer',{}, function(data) {
         var outputDiv = buttonClicked.parents(".panel-footer").siblings(".panel-body").children(".output");
         data = data.replace(/\n/g, "<br/>");
-        outputDiv.append(data+'<br />');
+        //outputDiv.append(data+'<br />');
+        outputDiv.append(getTimestamp()+": Disabling server...<br />");
         outputDiv.scrollTop(outputDiv[0].scrollHeight);
     });
 };
 
-var getStatusMinecraftServer = function(e){
-    $.get( '/getStatusMinecraftServer',{}, function(data) {
+var getStatusMinecraftServer = function(e) {
+    $.get('/getStatusMinecraftServer', {}, function (data) {
         if (typeof data === 'string' || data instanceof String) {
             //we got a string back, no worries
         } else { //error
@@ -73,9 +78,9 @@ var getStatusMinecraftServer = function(e){
         }
         var searchString = "Active: ";
         var startIndex = data.indexOf(searchString);
-        var activeFlag = data.charAt(startIndex+searchString.length); //should be a for active or f for failed
+        var activeFlag = data.charAt(startIndex + searchString.length); //should be a for active or f for failed
 
-        if (activeFlag == 'a'){ //service active
+        if (activeFlag == 'a') { //service active
             PAGE.statusMinecraftServerUnknown.hide();
             PAGE.statusMinecraftServerActive.show();
             PAGE.statusMinecraftServerOffline.hide();
@@ -91,9 +96,9 @@ var getStatusMinecraftServer = function(e){
 
         var searchString = "/etc/systemd/system/minecraftserver.service; ";
         var startIndex = data.indexOf(searchString);
-        var enabledFlag = data.charAt(startIndex+searchString.length); //should be a for active or f for failed
+        var enabledFlag = data.charAt(startIndex + searchString.length); //should be a for active or f for failed
 
-        if (enabledFlag == 'e'){ //service active
+        if (enabledFlag == 'e') { //service active
             PAGE.enabledMinecraftServerUnknown.hide();
             PAGE.enabledMinecraftServerEnable.show();
             PAGE.enabledMinecraftServerDisabled.hide();
@@ -106,6 +111,21 @@ var getStatusMinecraftServer = function(e){
             PAGE.enabledMinecraftServerEnable.hide();
             PAGE.enabledMinecraftServerDisabled.hide();
         }
-    });
-
+    })
 };
+
+var getTimestamp = function() {
+    var date = new Date();
+    //var year = date.getFullYear();
+    //var month = date.getMonth()+1;
+    //var day = date.getDate();
+    var hours = date.getHours();
+    if (hours < 10) {hours = '0'+hours;}
+    var minutes = date.getMinutes();
+    if (minutes < 10) {minutes = '0'+minutes;}
+    var seconds = date.getSeconds();
+    if (seconds < 10) {seconds = '0'+seconds;}
+
+    return ''+hours+':'+minutes+":"+seconds;
+};
+
