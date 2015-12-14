75c673a6fc2154abaaebcc7ee5c397ddbc1a40e9
Landon <landon.holmes@gmail.com>
2015-12-14 14:11:55 -0600

    cleaning up some missing spaces

--- helpers/pass.js
+++ helpers/pass.js
@@ -2,29 +2,32 @@ var crypto = require('crypto');
 var defaultLength = 128;
 var defaultIterations = 10000;
 
-exports.hash =  function(pass,callback) {
+exports.hash =  function(pass, callback) {
     crypto.randomBytes(defaultLength, function (err, salt) {
         if (err) {throw err;}
-        exports.hashWithOptions(pass,defaultIterations,salt,callback);
+        exports.hashWithOptions(pass, defaultIterations, salt, callback);
     });
 };
 
-exports.hashWithOptions =  function(pass,iterations,salt,callback) {
+exports.hashWithOptions =  function(pass, iterations, salt, callback) {
     crypto.pbkdf2(pass, salt, iterations, 256, function (err, hash) {
             if (err) { throw err; }
             var hashed = (new Buffer(hash).toString('hex'));
             salt = salt.toString('hex');
             var hashedPassword = iterations + ':' + salt + ':' + hashed;
-            callback(null,hashedPassword);
+            callback(null, hashedPassword);
     });
 };
 
-exports.validatePassword = function(attemptedPassword,storedPassword,callback) {
+exports.validatePassword = function(attemptedPassword, storedPassword, callback) {
     var parts = storedPassword.split(":"); //get the parts of the stored password
-    if (parts.length != 3) { console.log('comparison password does not have 3 parts');callback(false);} //crap out if the stored password does not have the right parts
+    if (parts.length != 3) { //crap out if the stored password does not have the right parts
+        console.log('comparison password does not have 3 parts');
+        callback(false);
+    }
     var storedPasswordIterations = parseInt(parts[0]); //get the stored password iterations
     var storedPasswordSalt = parts[1]; //get the stored password salt
-    exports.hashWithOptions(attemptedPassword,storedPasswordIterations,storedPasswordSalt,function(err,e){ //encode our attempted password in the same way as the stored one, then compare
-        callback(e==storedPassword)
+    exports.hashWithOptions(attemptedPassword, storedPasswordIterations, storedPasswordSalt, function(err, e){ //encode our attempted password in the same way as the stored one, then compare
+        callback(e == storedPassword);
     });
 };
